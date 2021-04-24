b293a256eb9e4462de7f9afc3e605f92268eaf1b
Waqas <wakassadik@gmail.com>
2021-04-24 12:35:55 +0500

    enchancing the css

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -5,7 +5,14 @@ import { Component, OnInit } from '@angular/core';
   template: '<p>success-alert works!</p>',
   //styleUrls: ['./success-alert.component.css']
   styles: [ 
-    `p {color: green;}`
+    `
+    p {
+      padding: 20px;
+      background-color: lightgreen;
+      border: 1px solid green;
+    }
+    
+    `
   ]
 })
 export class SuccessAlertComponent implements OnInit {
